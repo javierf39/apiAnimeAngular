38c2572b654d69f12e7788e8b7f7eb710d93d50c
javierf39 <javi.freire@duocuc.cl>
2022-10-28 12:13:06 -0400

    finalizado

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,6 +15,7 @@ export class HomeComponent implements OnInit {
   imgAnime:any;
   type!:string;
   studio!:string;
+  encontrado:boolean = true;
 
   constructor(private animesrv: AnimeService) { }
 
@@ -28,14 +29,26 @@ export class HomeComponent implements OnInit {
     this.imgAnime = '';
     if(this.nameAnime){
       $("#spinner").css('display', 'block');
+      $(".mensaje-vacio").css('display', 'none');
       this.animesrv.getAnime(this.nameAnime).pipe(
-        tap((anime:any) => {this.infoAnime = anime.data[0]
+        tap((anime:any) => {if(anime.data.length > 0){
+                            this.infoAnime = anime.data[0]
                             $("#spinner").css('display', 'none')
-                            this.type = anime.data[0].demographics[0].name
-                            this.studio =anime.data[0].studios[0].name
+                            this.type = anime.data[0].demographics.length > 0 ? anime.data[0].demographics[0].name : null
+                            this.studio = anime.data[0].studios.length > 0 ? anime.data[0].studios[0].name : null
                             this.imgAnime = anime.data[0].images.jpg.image_url
-                            console.log(anime.data[0])})
+                            this.encontrado = true;
+                            console.log(anime.data[0])
+                          }
+                            else{
+                              $("#spinner").css('display', 'none')
+                              this.encontrado = false;
+                            }
+                            })
       ).subscribe()
+    }else{
+      $(".mensaje-vacio").css('display', 'block');
+      this.encontrado=true
     };
 
    
